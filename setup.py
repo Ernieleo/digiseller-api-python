969766f036043fabb304aa1ec1e4772cc6e50cd2
Ernieleo <mmetelnikov57@gmail.com>
2024-06-01 06:08:58 +0300

    Обновление данных

--- setup.py
+++ setup.py
@@ -8,23 +8,25 @@ def readme():
 
 setup(
     name='digiseller_api_python',
-    version='1.4.1',
+    version='1.4.2',
     author='Ernieleo',
     author_email='ernieleo@vk.com',
-    description='Interaction with Digiseller API',
+    description='Interaction with Digiseller API via Python',
     long_description=readme(),
     long_description_content_type='text/markdown',
     url='https://github.com/Ernieleo/Digiseller-API-Python',
     packages=find_packages(),
     install_requires=['requests>=2.31.0'],
     classifiers=[
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ],
-    keywords='api client',
+    keywords='digiseller api python',
     project_urls={
-        'Documentation': 'https://my.digiseller.com/inside/api.asp'
+        'Documentation': 'https://my.digiseller.com/inside/api.asp',
+        'Download': 'https://pypi.org/project/digiseller-api-python/',
+        'Write me': 'https://t.me/ernieleo'
     },
-    python_requires='>=3.7'
+    python_requires='>=3.8'
 )
