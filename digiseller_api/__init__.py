c401a5a81be627ff4d195910f1c2307480df8578
Ernieleo-V <mmetelnikov57@gmail.com>
2024-10-16 03:07:10 +0300

    Init

---
+++ digiseller_api/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Ernieleo"
+__email__ = "mmetelnikov57@gmail.com"
+__description__ = "Interaction with Digiseller API via Python"
+
+from .base_api import DigisellerApi
+__all__ = ["DigisellerApi"]
