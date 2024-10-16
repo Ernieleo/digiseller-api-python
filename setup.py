5b1b5233fc9a2753669c4899d1e55749eef32b47
Ernieleo-V <mmetelnikov57@gmail.com>
2024-10-16 22:25:39 +0300

    Исправление кодировки в setup.py: добавлен параметр encoding='utf-8' для файла README.md
    - Указана кодировка 'utf-8' при чтении README.md, чтобы избежать ошибок с кодировкой на Windows.
    - Это изменение позволяет корректно устанавливать пакет через pip с использованием репозитория GitHub, устраняя проблему UnicodeDecodeError.

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md', 'r') as f:
+    with open('README.md', 'r', encoding='utf-8') as f:
         return f.read()
-
 
 setup(
     name='digiseller_api_python',
