e6393e908abef97681aeff84be366742f037a411
Ernieleo-V <mmetelnikov57@gmail.com>
2024-11-20 18:49:06 +0300

    Исправление ошибки и добавление проверок

--- digiseller_api/request_handler.py
+++ digiseller_api/request_handler.py
@@ -5,27 +5,36 @@ import json
 def send_request(method, url, **kwargs):
     if 'files' in kwargs:
         headers = {'Accept': 'application/json; charset=UTF-8'}
-    else:
-        headers = {'Accept': 'application/json; charset=UTF-8', 'Content-Type': 'application/json'}
-    kwargs['headers'] = headers
+        kwargs['headers'] = headers
 
-    with httpx.Client() as client:
+    with httpx.Client(timeout=10) as client:
         response = client.request(method, url, **kwargs)
+        # response.raise_for_status()
 
-        # Check if the response contains JSON
-        if response.headers.get("Content-Type") == "application/json; charset=utf-8" or response.headers.get("Content-Type") == "application/json":
-            try:
-                return response.json()
-            except json.JSONDecodeError:
-                raise ValueError(f"Error decoding JSON, response received: {response.text}")
+        if response.status_code <= 200:
 
-        # If Content-Type is not JSON
-        else:
-            if response.headers.get("Content-Type", "").startswith("image/"):
-                # The response contains image data
+            content_type = response.headers.get("Content-Type", "")
+
+            # Обработка JSON ответа
+            if content_type.startswith("application/json"):
+                try:
+                    return response.json()
+                except json.JSONDecodeError:
+                    raise ValueError(f"Error decoding JSON, response received: {response.text}")
+
+            # Обработка XML ответа (оставляю как есть)
+            elif content_type.startswith("application/xml") or content_type.startswith("text/xml"):
+                return response.text
+
+            # Обработка изображений
+            elif content_type.startswith("image/"):
                 return response.content
 
+            # Обработка текста
             elif response.text:
-                # The response contains text data
                 return response.text
+
             return response.status_code
+
+        else:
+            raise ValueError(f"Response with a bad http code was received: {response.status_code} / {response.text}")
