15a4664a63bc33709dd0eba6acfdd77b194106e9
Ernieleo-V <dev@offnik.ru>
2025-03-22 03:27:39 +0300

    Удалены либо перемещены

--- digiseller_api/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = "Ernieleo"
-__email__ = "mmetelnikov57@gmail.com"
-__description__ = "Interaction with Digiseller API via Python"
-
-from .base_api import DigisellerApi
-__all__ = ["DigisellerApi"]
