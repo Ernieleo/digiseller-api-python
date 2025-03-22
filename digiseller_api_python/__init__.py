03f004c75b6bcae82c8a5a4f72568943b4dd77ef
Ernieleo-V <dev@offnik.ru>
2025-03-22 03:42:02 +0300

    Добавлены exceptions

---
+++ digiseller_api_python/__init__.py
@@ -0,0 +1,10 @@
+from ._base_api import DigisellerApi
+from ._exceptions import *
+
+__all__ = [
+    "DigisellerApi",
+    "DigisellerError",
+    "DigisellerTimeoutError",
+    "DigisellerInvalidResponseError",
+    "DigisellerHTTPError",
+]
---
+++ digiseller_api_python/_exceptions.py
@@ -0,0 +1,20 @@
+class DigisellerError(Exception):
+    """Базовый класс для всех ошибок, связанных с Digiseller API."""
+    pass
+
+
+class DigisellerTimeoutError(DigisellerError):
+    """Ошибка тайм-аута при запросе к API Digiseller."""
+    pass
+
+
+class DigisellerInvalidResponseError(DigisellerError):
+    """Ошибка при получении некорректного ответа от API Digiseller."""
+    pass
+
+
+class DigisellerHTTPError(DigisellerError):
+    """Ошибка HTTP с указанием кода статуса."""
+    def __init__(self, status_code, message):
+        super().__init__(f"HTTP {status_code}: {message}")
+        self.status_code = status_code
