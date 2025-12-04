a07ed7818b07fecfd2ad0e620bbb720a2e3e59a8
Ernieleo-V <git@offnik.ru>
2025-12-04 21:05:12 +0300

    добавлено управление таймаутом и прокси в send_request
    Расширение функционала метода `send_request`:
    - Добавлены параметры `timeout` (по умолчанию 60 сек.) и `proxy`.
    - Улучшена обработка заголовков: предусмотрена настройка дефолтных значений и дополнение существующих.
    - Реализована новая обработка статусов ответа:
      - Добавлены исключения `DigisellerAPIAuthError` для кодов 401 и 403.
      - Улучшена проверка содержимого ответа: различение форматов JSON, HTML, изображений и других данных.
      - Исправлена и дополнена обработка HTML-ответов для улучшения диагностики ошибок.
    - Добавлена обработка статуса ответа 204 с возвращением значения `{"success": True}`.
    - Реализована логика обработки HTML при ошибках сервера Digiseller, включая проверку на технические работы.
    - Минимизирована вероятность ошибок за счёт улучшения обработки исключений `httpx`.
    
    Fix
    Closes #2

--- digiseller_api_python/_request_handler.py
+++ digiseller_api_python/_request_handler.py
@@ -5,23 +5,37 @@ from ._exceptions import (
     DigisellerTimeoutError,
     DigisellerInvalidResponseError,
     DigisellerHTTPError,
+    DigisellerUnavailableError,
+    DigisellerAPIAuthError
 )
 
 
-def send_request(method, url, **kwargs):
+def send_request(method, url: str, timeout: int = 60, proxy: str = None, **kwargs):
 
-    kwargs['headers'] = {"Accept": "application/json, application/xml;q=0.9, text/xml;q=0.8, */*;q=0.7"}
+    default_headers = {"Accept": "application/json, application/xml;q=0.9, text/xml;q=0.8, */*;q=0.7"}
 
     if 'files' in kwargs:
-        headers = {'Accept': 'application/json'}
-        kwargs['headers'] = headers
+        default_headers = {'Accept': 'application/json'}
+
+    # Инициализируем заголовки, если их нет, или дополняем существующие
+    if 'headers' not in kwargs:
+        kwargs['headers'] = default_headers
+    else:
+        for key, value in default_headers.items():
+            kwargs['headers'].setdefault(key, value)
 
     try:
-        with httpx.Client(timeout=10) as client:
+        with httpx.Client(timeout=timeout, proxy=proxy) as client:
+            #print(f'Sending request to {url}...')
             response = client.request(method, url, **kwargs)
-            if response.status_code == 200:
+            content_type = response.headers.get("Content-Type", "")
 
-                content_type = response.headers.get("Content-Type", "")
+            if response.status_code in (401, 403):
+                raise DigisellerAPIAuthError(
+                    "Access denied. Check your API key access permissions and try again..")
+
+            elif response.status_code in (200, 400):
+                #print(f"Received a {response.status_code} response from Digiseller. {response.url}")
 
                 # Обработка JSON ответа
                 if content_type.startswith("application/json"):
@@ -38,13 +52,44 @@ def send_request(method, url, **kwargs):
                 elif content_type.startswith("image/"):
                     return response.content
 
-                # Обработка текста
+                # HTML ловим
+                elif content_type.startswith("text/html"):
+                    raise DigisellerInvalidResponseError(
+                        f"Received unexpected HTML content. "
+                        f"Check the URL and parameters. Preview:\n{response.text[:300]}"
+                    )
+
+                # Всё остальное
                 elif response.text:
                     return response.text
 
-                return response.status_code
+                else:
+                    return response.status_code
+
+            elif response.status_code == 204:
+                return {"success": True}
 
             else:
+                # Сервер вернул HTML
+                if content_type.startswith("text/html"):
+                    text = response.text.strip()
+
+                    if any(keyword in text for keyword in ("Digiseller UPDATING", "404 - File or directory not found", "Server Error", "Digiseller is experiencing an unscheduled maintenance work")):
+                        raise DigisellerUnavailableError(
+                            f"Digiseller is likely undergoing maintenance.\n"
+                            f"Response code: {response.status_code}\n"
+                            f"URL: {response.url}\n"
+                            f"Preview:\n{text[:300]}"
+                        )
+                    else:
+                        raise DigisellerInvalidResponseError(
+                            f"An unexpected HTML response was received. Perhaps the path or parameters are incorrect.\n"
+                            f"Response code: {response.status_code}\n"
+                            f"URL: {response.url}\n"
+                            f"Preview:\n{text[:300]}"
+                        )
+
+                # Если не HTML
                 raise DigisellerHTTPError(response.status_code, response.text)
 
     except httpx.TimeoutException:
